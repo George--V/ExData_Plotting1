d46259d61bbc6e4fe27179495808fc86965a7585
george--v <g3ovalle@gmail.com>
2014-05-11 21:34:56 +0100

    plot4
    plot4 and updates to plot3

--- plot4.R
+++ plot4.R
@@ -8,7 +8,23 @@ y<-subset(x,Date=="1/2/2007" | Date=="2/2/2007")
 times<-strptime(paste(as.character.Date(y$Date,format="%d/%m/%Y"),y$Time), 
                 format="%d/%m/%Y %H:%M:%S")
 
-png("plot3.png", width=480, height=480)
+png("plot4.png", width=480, height=480)
+
+# Split screen
+
+par(mfrow=c(2,2))
+
+# Plot 1
+
+plot(times, y$Global_active_power,type="l",
+     ylab="Global Active Power(kilowatts)",xlab="")
+
+# Plot 2
+
+plot(times, y$Voltage,type="l",
+     ylab="Voltage",xlab="Datetime",ylim=c(234,246))
+
+# Plot 3
 
 with(y, plot(times, Sub_metering_1, ylab="Energy sub metering", xlab="",
              type="l", ylim=c(0,40)))
@@ -20,4 +36,8 @@ with(y, plot(times, Sub_metering_3, ylab="Energy sub metering", xlab="",
              type="l", ylim=c(0,40), col="Blue"))
 legend("topright", pch = "_", col = c("black","blue", "red"), 
        legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
+# Plot 4
+
+plot(times, y$Global_reactive_power,type="l",
+     ylab="Global_reactive_power",xlab="Datetime",ylim=c(0.0,0.5))
 dev.off()
